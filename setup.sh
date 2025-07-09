2fe95cf3802d1f1126486de2df5d4fa8cb3d1602
gmarchetti2020 <59297378+gmarchetti2020@users.noreply.github.com>
2025-07-09 15:25:03 -0700

    changed config

--- setup.sh
+++
@@ -1,5 +0,0 @@
-# The order is important
-sudo sh -c "echo always > /sys/kernel/mm/transparent_hugepage/enabled"
-pip install jax[tpu] tensorflow-cpu keras-hub sentencepiece google-cloud-aiplatform
-pip install torch torchvision torchaudio  --index-url https://download.pytorch.org/whl/cpu
-pip install transformers accelerate
